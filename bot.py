2259ffc2944266f238b47c4d5e109860ef46121c
Jay684 <jaymasters.is.me@gmail.com>
2025-01-01 12:59:28 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+from main import gen_pass
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.event
+async def on_member_join(member):
+    guild = member.guild
+    if guild.system_channel is not None:
+        to_send = f'Welcome {member.mention} to {guild.name}!'
+        await guild.system_channel.send(to_send)
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh=5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def gen(ctx, count_heh=5):
+    await ctx.send(gen_pass(count_heh))
+
+bot.run("YOUR_TOKEN")
